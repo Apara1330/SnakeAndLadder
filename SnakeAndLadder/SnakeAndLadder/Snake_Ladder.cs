8b85b31f1c17d884ae562beb2f5a17bc0e884052
Apara1330 <polapara1330@gmail.com>
2022-02-22 14:06:22 +0530

    [Apara]- Player checks for an option

--- SnakeAndLadder/SnakeAndLadder/Snake_Ladder.cs
+++ SnakeAndLadder/SnakeAndLadder/Snake_Ladder.cs
@@ -9,13 +9,31 @@ namespace SnakeAndLadder
     public class Snake_Ladder
     {
         int FirstPlayerPosition;
+        int ranOption;
 
         Random random = new Random();
-
-        public void GameBegins()
+        public void game()
         {
-            FirstPlayerPosition = 0;//1st player starting position zero
-            int DiceValue = random.Next(1,7);
+            FirstPlayerPosition = 0; //First player starting at 0th position
+
+            int DiceValue = random.Next(1, 7);//Taking random value of Dice from 1-6
+            ranOption = random.Next(0, 3);
+
+            if (ranOption == 0)
+            {
+                Console.WriteLine("no play");
+                FirstPlayerPosition += FirstPlayerPosition;
+            }
+            else if (ranOption == 1)
+            {
+                Console.WriteLine("Ladder");
+                FirstPlayerPosition = FirstPlayerPosition + DiceValue;
+            }
+            else
+            {
+                Console.WriteLine("Snake");
+                FirstPlayerPosition = FirstPlayerPosition + DiceValue;
+            }
         }
     }
 }
