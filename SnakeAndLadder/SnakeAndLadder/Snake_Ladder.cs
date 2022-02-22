1df9c52d0c8c6dd868a98ede7754ff14acda99e3
Apara1330 <polapara1330@gmail.com>
2022-02-22 12:52:39 +0530

    [Apara]-Game started with single player

---
+++ SnakeAndLadder/SnakeAndLadder/Snake_Ladder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+     class Snake_Ladder
+    {
+        int FirstPlayerPosition;
+
+        public void GameBegins()
+        {
+            FirstPlayerPosition = 0;
+
+        }
+    }
+}
