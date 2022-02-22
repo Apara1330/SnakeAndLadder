c5b56f1445cba581a8383fc25bc7cad58ab21e87
Apara1330 <polapara1330@gmail.com>
2022-02-22 14:29:10 +0530

    [Apara]- Repeat till player reaches winning position 100

--- SnakeAndLadder/SnakeAndLadder/Snake_Ladder.cs
+++ SnakeAndLadder/SnakeAndLadder/Snake_Ladder.cs
@@ -10,29 +10,38 @@ namespace SnakeAndLadder
     {
         int FirstPlayerPosition;
         int ranOption;
+        int Max_Position = 100;//last position in the game
 
-        Random random = new Random();
-        public void game()
+        Random random = new Random();//to get random values
+        public void GameBegins()
         {
             FirstPlayerPosition = 0; //First player starting at 0th position
+            while (FirstPlayerPosition <= Max_Position)//to reach maximum value
+            {
+               
+                int DiceValue = random.Next(1, 7); //Taking random value of Dice from 1-6
 
-            int DiceValue = random.Next(1, 7);//Taking random value of Dice from 1-6
-            ranOption = random.Next(0, 3);
+                ranOption = random.Next(0, 3);
 
-            if (ranOption == 0)
-            {
-                Console.WriteLine("no play");
-                FirstPlayerPosition += FirstPlayerPosition;
-            }
-            else if (ranOption == 1)
-            {
-                Console.WriteLine("Ladder");
-                FirstPlayerPosition = FirstPlayerPosition + DiceValue;
-            }
-            else
-            {
-                Console.WriteLine("Snake");
-                FirstPlayerPosition = FirstPlayerPosition + DiceValue;
+                if (ranOption == 0)
+                {
+                    Console.WriteLine("Not played");
+                    FirstPlayerPosition = FirstPlayerPosition + FirstPlayerPosition;
+                }
+                else if (ranOption == 1)
+                {
+                    Console.WriteLine("Ladder");
+                    FirstPlayerPosition = FirstPlayerPosition + DiceValue;
+                }
+                else if (ranOption == 2)
+                {
+                    Console.WriteLine("Snake");
+                    FirstPlayerPosition = FirstPlayerPosition - DiceValue;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move");
+                }
             }
         }
     }
